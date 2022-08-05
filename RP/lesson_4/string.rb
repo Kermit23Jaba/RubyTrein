64354dd70de8e4a3880dc5640de5b3fbd7722764
Kermit23Jaba <aurfree@gmail.com>
2022-08-05 13:32:42 +0500

    05.08.2022

--- RP/lesson_4/string.rb
+++ RP/lesson_4/string.rb
@@ -2,4 +2,4 @@ x = gets.chomp
 y = gets.to_i
 z = gets.to_f
 
-puts "x = #{x}, y = #{y}, z = #{z}"
+puts "x = #{x.to_s}, y = #{y.to_s}, z = #{z.to_s}"
---
+++ RP/lesson_5/string.rb
@@ -0,0 +1,8 @@
+# 5.upto(15) { |i| puts i }
+
+# 50.downto(-50) { |i| puts i }
+
+name = gets.strip.capitalize
+
+puts "Your name: #{name}"
+puts "Lot a word: #{name.size}"
